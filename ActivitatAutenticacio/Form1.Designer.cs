cd97dd58ff2ce3694fd48f6bb7bc7eaf099a9ff8
bymarkes <davidmarques.dp@gmail.com>
2016-11-28 17:10:10 +0100

    Add project files.

---
+++ ActivitatAutenticacio/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ActivitatAutenticacio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.logPass = new System.Windows.Forms.TextBox();
+            this.signPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.signName = new System.Windows.Forms.TextBox();
+            this.signPass2 = new System.Windows.Forms.TextBox();
+            this.logButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.signUpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // logName
+            // 
+            this.logName.Location = new System.Drawing.Point(89, 54);
+            this.logName.Name = "logName";
+            this.logName.Size = new System.Drawing.Size(100, 20);
+            this.logName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOG IN";
+            // 
+            // logPass
+            // 
+            this.logPass.Location = new System.Drawing.Point(89, 94);
+            this.logPass.Name = "logPass";
+            this.logPass.PasswordChar = '*';
+            this.logPass.Size = new System.Drawing.Size(100, 20);
+            this.logPass.TabIndex = 2;
+            // 
+            // signPass
+            // 
+            this.signPass.Location = new System.Drawing.Point(89, 220);
+            this.signPass.Name = "signPass";
+            this.signPass.PasswordChar = '*';
+            this.signPass.Size = new System.Drawing.Size(100, 20);
+            this.signPass.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(100, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "SIGN UP";
+            // 
+            // signName
+            // 
+            this.signName.Location = new System.Drawing.Point(89, 180);
+            this.signName.Name = "signName";
+            this.signName.Size = new System.Drawing.Size(100, 20);
+            this.signName.TabIndex = 3;
+            // 
+            // signPass2
+            // 
+            this.signPass2.Location = new System.Drawing.Point(89, 259);
+            this.signPass2.Name = "signPass2";
+            this.signPass2.PasswordChar = '*';
+            this.signPass2.Size = new System.Drawing.Size(100, 20);
+            this.signPass2.TabIndex = 6;
+            // 
+            // logButton
+            // 
+            this.logButton.Location = new System.Drawing.Point(244, 71);
+            this.logButton.Name = "logButton";
+            this.logButton.Size = new System.Drawing.Size(75, 23);
+            this.logButton.TabIndex = 7;
+            this.logButton.Text = "LOG IN";
+            this.logButton.UseVisualStyleBackColor = true;
+            this.logButton.Click += new System.EventHandler(this.logButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "User";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 266);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Password";
+            // 
+            // signUpButton
+            // 
+            this.signUpButton.Location = new System.Drawing.Point(244, 217);
+            this.signUpButton.Name = "signUpButton";
+            this.signUpButton.Size = new System.Drawing.Size(75, 23);
+            this.signUpButton.TabIndex = 14;
+            this.signUpButton.Text = "SIGNUP";
+            this.signUpButton.UseVisualStyleBackColor = true;
+            this.signUpButton.Click += new System.EventHandler(this.signUpButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 322);
+            this.Controls.Add(this.signUpButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.logButton);
+            this.Controls.Add(this.signPass2);
+            this.Controls.Add(this.signPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.signName);
+            this.Controls.Add(this.logPass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox logName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox logPass;
+        private System.Windows.Forms.TextBox signPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox signName;
+        private System.Windows.Forms.TextBox signPass2;
+        private System.Windows.Forms.Button logButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button signUpButton;
+    }
+}
+
---
+++ ActivitatAutenticacio/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace ActivitatAutenticacio
+{
+    public partial class Form1 : Form
+    {
+        const String filepath = @"C:\Users\DAM05\Desktop\usersLog.txt"; //Ruta especifica de l'arxiu on es guarden usuari, salt i hash de la contrasenya.
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Comprovem que l'arxiu existeixi, sino el creem
+            if (!(File.Exists(filepath)))
+            {
+                using (File.Create(filepath)) ; //Creem l'arxiu
+            }
+        }
+
+        private void signUpButton_Click(object sender, EventArgs e)
+        {
+            if (signName.Text == "" || signPass.Text == "" || signPass2.Text == "") //Comprovem que els camps no estiguin buits
+            {
+                MessageBox.Show("Has d'omplir tots els camps");
+            }
+            else if (signPass.Text != signPass2.Text) //Comprovem que els camps de les contrasenyes coincideixin
+            {
+                MessageBox.Show("Les contrasenyes han de coincidir");
+            }
+            else
+            {
+                String username;
+                String password;
+                //Asignem cada camp a una variable
+                username = signName.Text;  
+
+                password = signPass.Text;
+
+                signIn(username, password);
+
+            }
+        }
+
+        private void signIn(String username, String password)
+        {
+            //Generem un salt
+            byte[] salt = doSalt();
+            //Generem un hash de la contrassenya
+            string hash = doHash(password, salt);
+
+            String saltConverted = Convert.ToBase64String(salt);
+
+            String newUser = username + "," + saltConverted + "," + hash; //Conquetenem tota la info
+            
+            //Guardem tota la informació al arxiu
+            System.IO.File.AppendAllText(filepath, newUser);
+            MessageBox.Show("Usuari creat correctament.");
+
+        }
+
+        private static byte[] doSalt()
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            return salt;
+        }
+
+        private static string doHash(string password, byte[] salt)
+        {
+            var hsh = new Rfc2898DeriveBytes(password, salt, 1000);
+            byte[] hash = hsh.GetBytes(32);
+            return Convert.ToBase64String(hash);
+        }
+
+        private void logButton_Click(object sender, EventArgs e)
+        {
+            if (logName.Text == "" || logPass.Text == "") //Comprovem que els camps no es trobin buits
+            {
+                MessageBox.Show("Has d'omplir tots els camps");
+            }
+            else
+            {
+                LogIn(logName.Text, logPass.Text);
+            }
+        }
+
+        private void LogIn(string nomUsuari, string passUsuari)
+        {
+            string usuari = buscarUsuari(nomUsuari); //Busquem si l'usuari introduït existeix
+            string msgbox = verificarLogIn(usuari, passUsuari);
+            MessageBox.Show(msgbox);
+        }
+
+        private string buscarUsuari(string nomUsuari)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filepath))
+                {
+                    string linia;
+                    string[] usuario;
+
+                    while ((linia = sr.ReadLine()) != null)//While que llegeix linia per linia l'arxiu
+                    {
+                        //Llegeix fins trobar una ',' (El nom d'usuari), (Salt), (Hash)
+                        usuario = linia.Split(',');
+
+                        if (usuario[0] == nomUsuari)
+                        {
+                            return usuario[0] + ',' + usuario[1] + ',' + usuario[2];
+                        }
+                    }
+                }
+                return "No s'ha trobat l'usuari.";
+            }
+            catch
+            {
+                return "ErrorAlLlegir";
+            }
+        }
+        public static string verificarLogIn(string usuariArxiu, string passUsuari)
+        {
+            string[] usuari;
+
+            byte[] saltArxiu;
+
+            if (usuariArxiu == "No s'ha trobat l'usuari.")
+            {
+                return "No s'ha trobat l'usuari.";
+            }
+            else if (usuariArxiu == "No s'ha trobat l'usuari.")
+            {
+                return "Hi ha hagut un error i no s'ha pogut llegir l'arxiu";
+            }
+            else
+            {
+                usuari = usuariArxiu.Split(',');
+
+                saltArxiu = Convert.FromBase64String(usuari[1]);
+
+                //Calculem el hash amb la salt guardada i la contrasenya entrada
+                string hash = doHash(passUsuari, saltArxiu);
+
+                //Comparem el hash guardat amb el generat
+                if (hash == usuari[2])
+                {
+                    return "Usuari identificat correctament.";
+                }
+                else
+                {
+                    return "La contrasenya no coincideix.";
+                }
+
+            }
+        }
+    }
+}
